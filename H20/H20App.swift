93b5de70658f9b2eba760e52f90df50a26661304
b0kch01 <nath49ers@gmail.com>
2021-07-28 16:55:26 -0700

    Initial Commit

---
+++ H20/H20App.swift
@@ -0,0 +1,17 @@
+//
+//  H20App.swift
+//  H20
+//
+//  Created by Nathan Choi on 7/28/21.
+//
+
+import SwiftUI
+
+@main
+struct H20App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
